4149d291c2d5991251434b37237f03f4e645ccf1
Edward Ao <87841176+EdwardAo@users.noreply.github.com>
2021-08-09 12:09:31 -0700

    Finished and Edited

---
+++ tiy5.py
@@ -0,0 +1,6 @@
+guests=['Darren', 'Chad', 'Chris']
+print("There's a person that can't make it that being",guests[0])
+guests[0]="Michelle"
+print('Please come over for dinner',guests[0])
+print('Please come over for dinner',guests[1])
+print('Please come over for dinner',guests[2])
---
+++ tiy6.py
@@ -0,0 +1,11 @@
+guests=['Michelle', 'Chad', 'Chris']
+print("Hey I found a bigger table",guests)
+guests.insert(0, 'Joseph')
+guests.insert(1, 'MIllah')
+guests.insert(2, 'Katy')
+print('Please come over for dinner',guests[0])
+print('Please come over for dinner',guests[1])
+print('Please come over for dinner',guests[2])
+print('Please come over for dinner',guests[3])
+print('Please come over for dinner',guests[4])
+print('Please come over for dinner',guests[5])
